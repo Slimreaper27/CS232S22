24fa663bd9bdf59c7ae9c387b2996da38d4e1241
Austin <69426895+Slimreaper27@users.noreply.github.com>
2022-02-19 04:14:15 +0000

    Completed Task2

--- assign3/task2/convert_2.c
+++ assign3/task2/convert_2.c
@@ -5,7 +5,46 @@
 //2) No globals allowed.
 char* convert_2(int dec)
 {
-	//TODO: your implementation
+  char* s = malloc(sizeof(char)*42);
+  if (dec < 0) {
+    dec = 2147483648 + dec;
+    for (int i = 2; i < 41; i++) {
+    if (dec % 2 == 1) {
+      s[42 - i] = '1';
+    }
+    else {
+      s[42 - i] = '0';
+    }
+    dec /= 2;
+    if (i % 5 == 0) {
+      s[41 - i] = ' ';
+      i++;
+    }
+  }
+    s[2] = '1';
+  }
+  else {
+    for (int i = 2; i < 41; i++) {
+    if (dec % 2 == 1) {
+      s[42 - i] = '1';
+    }
+    else {
+      s[42 - i] = '0';
+    }
+    dec /= 2;
+    if (i % 5 == 0) {
+      s[41 - i] = ' ';
+      i++;
+    }
+  }
+  }
+  
+  
+  s[0] = '0';
+  s[1] = 'b';
+  s[41] = '\0';
+  
+	return s;
 }
 
 int main() {
@@ -14,6 +53,6 @@ int main() {
 	printf("Enter the Decimal Number\n");
 	scanf("%d",&n);
 	bin = convert_2(n);
-	printf("The Binary Notation of %d is\t %s\n", n, bin);
-	//TODO: do we need to release the memory of bin?
+	printf("The Binary Notation of %d is\t %s \n", n, bin);
+	free(bin);
 }
